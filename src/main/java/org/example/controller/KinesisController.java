95493bc5cf61fbfaaa761380446f877b663e7b00
HalomkoDmytro <galomko.dmytro@gmail.com>
2025-09-19 14:38:58 +0300

    shardIteratorType

--- src/main/java/org/example/controller/KinesisController.java
+++ src/main/java/org/example/controller/KinesisController.java
@@ -52,6 +52,14 @@ public class KinesisController {
         return ResponseEntity.ok(response);
     }
 
+    /**
+     * @param shardIteratorType options:
+     * AT_SEQUENCE_NUMBER - Start reading from the position denoted by a specific sequence number, provided in the value StartingSequenceNumber.
+     * AFTER_SEQUENCE_NUMBER - Start reading right after the position denoted by a specific sequence number, provided in the value StartingSequenceNumber.
+     * AT_TIMESTAMP - Start reading from the position denoted by a specific time stamp, provided in the value Timestamp.
+     * TRIM_HORIZON - Start reading at the last untrimmed record in the shard in the system, which is the oldest data record in the shard.
+     * LATEST - Start reading just after the most recent record in the shard, so that you always read the most recent data in the shard.
+     */
     @GetMapping("/read")
     public ResponseEntity<List<Map<String, Object>>> readMessages(
             @RequestParam(defaultValue = "TRIM_HORIZON") String shardIteratorType) {
